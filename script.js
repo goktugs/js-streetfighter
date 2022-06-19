c398e0a251057292e8ca5b5e3998e282e2e22e0c
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-19 09:22:10 +0300

    Add First Image

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const GameViewPort = {
+  WIDTH: 384,
+  HEIGHT: 224,
+};
+
 window.onload = function () {
   const canvasEl = document.querySelector('canvas');
   const context = canvasEl.getContext('2d');
 
-  canvasEl.width = 384;
-  canvasEl.height = 224;
+  canvasEl.width = GameViewPort.WIDTH;
+  canvasEl.height = GameViewPort.HEIGHT;
+
+  const ken = document.querySelector('img');
+
+  context.strokeStyle = 'yellow';
+  context.moveTo(0, 0);
+  context.lineTo(GameViewPort.WIDTH, GameViewPort.HEIGHT);
+  context.moveTo(GameViewPort.WIDTH, 0);
+  context.lineTo(0, GameViewPort.HEIGHT);
+  context.stroke();
+
+  context.drawImage(ken, 0, 0);
 
   console.log(context);
 };
